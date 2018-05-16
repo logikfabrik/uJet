bc0c89df7eb59718a59e35d64a6eda8b1930fbf0
logikfabrik <anton@logikfabrik.se>
2018-05-16 20:01:43 +0200

    Added unit tests

---
+++ src/Logikfabrik.Umbraco.Jet.Test/ContainerTest.cs
@@ -0,0 +1,72 @@
+﻿// <copyright file="ContainerTest.cs" company="Logikfabrik">
+//   Copyright (c) 2016 anton(at)logikfabrik.se. Licensed under the MIT license.
+// </copyright>
+
+namespace Logikfabrik.Umbraco.Jet.Test
+{
+    using Shouldly;
+    using Xunit;
+
+    public class ContainerTest
+    {
+        private interface IService
+        {
+        }
+
+        [Fact]
+        public void CanResolveByContractType()
+        {
+            var container = new Container();
+
+            container.Register<IContainer, Container>();
+
+            var resolved = container.Resolve<IContainer>();
+
+            resolved.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void CanResolveByImplementationType()
+        {
+            var container = new Container();
+
+            container.Register<IContainer, Container>();
+
+            var resolved = container.Resolve<Container>();
+
+            resolved.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void CanResolveByInstanceWithImplicitContract()
+        {
+            var instance = new Service();
+
+            var container = new Container();
+
+            container.Register(instance);
+
+            var resolved = container.Resolve<Service>();
+
+            resolved.ShouldBeSameAs(instance);
+        }
+
+        [Fact]
+        public void CanResolveByInstanceWithExplicitContract()
+        {
+            var instance = new Service();
+
+            var container = new Container();
+
+            container.Register<IService>(instance);
+
+            var resolved = container.Resolve<IService>();
+
+            resolved.ShouldBeSameAs(instance);
+        }
+
+        private class Service : IService
+        {
+        }
+    }
+}
